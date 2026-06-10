50ace99254e873885c9a5fa6cb43c93da492ef22
Afz-oos <latavee17200@gmail.com>
2026-06-10 23:47:46 +0700

    Update DE KAITUN.lua

--- DE KAITUN.lua
+++ DE KAITUN.lua
@@ -27,7 +27,7 @@ if EnableWhiteScreen then
         gui.IgnoreGuiInset = true
         gui.ResetOnSpawn = false
         local frame = Instance.new("Frame")
-        frame.BackgroundColor3 = Color3.new(1, 1, 1)
+        frame.BackgroundColor3 = Color3.new(0, 0, 0)
         frame.Size = UDim2.new(1, 0, 1, 0)
         frame.Parent = gui
         pcall(function()
